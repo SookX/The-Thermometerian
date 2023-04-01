8080a8b4bb88551b6efe23bae0a9ab471682afc7
SookX <vagrivas08@gmail.com>
2023-04-02 01:16:20 +0300

    Home page

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = '63103453574bccae5541fa05'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
